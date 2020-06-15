a5963bdd90b076c2bf96d5ca11f2f1563a868465
MaherHassanain <maher_hassanain@hotmail.com>
2020-06-15 11:10:50 -0400

    demo code for a single yeast study

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fs = require('fs');
+const CSVToJSON = require('csvtojson');
 var indexRouter = require('./routes/index');
-
+const sample = require('./routes/sample');
+// console.log(sample);
 var app = express();
 
 // view engine setup
@@ -35,5 +37,28 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// CSVToJSON().fromFile("./de_expression/GSE4136_YEAST_EXAMPLE_DE.csv").then(source => {
+//   // console.log(source);
+//   source.push({
+//     "adj.P.Val" : "0.000137538508078003",
+//     "P.Value" : "2.29827749126127e-08",
+//     "t" : "-31.3039813536524",
+//     "B" : "9.60606491713567",
+//     "logFC" : "-3.64977447963713",
+//     "Gene_symbol" : "AAH1",
+//     "Platform_ORF" : "YNL141W",
+//     "Gene_title" : "adenine deaminase",
+//     "GO_Function" : "adenine deaminase activity///adenine deaminase activity///adenine deaminase activity///deaminase activity///hydrolase activity///metal ion binding///zinc ion binding",
+//     "GO_Process" : "adenine catabolic process///adenine catabolic process///adenine catabolic process///hypoxanthine salvage///hypoxanthine salvage///hypoxanthine salvage///nucleotide metabolic process///purine ribonucleoside monophosphate biosynthetic process///purine-containing compound salvage///purine-containing compound salvage",
+//     "GO_Component" : "cytoplasm///cytoplasm///nucleus///nucleus",
+//     "Chromosome_annotation" : "Chromosome XIV, NC_001146.8 (359596..360639)"
+//
+//   });
+//   var data = JSON.stringify(source);
+//   fs.writeFileSync('./JSON/GSE4136_YEAST.json', data);
+// });
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,56 @@ const collection = "yeastGenes";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+  // insert_GSE4136_Yeast_Data();
+  // insertNewColumn_GSE4136();
+  addAccessionGSE4136();
+  mongo.connect(url,function(err, client) {
+    assert.equal(null,err);
+    var db = client.db('igemConcordia2020');
+    db.collection(collection).find({}).toArray((err, documents) => {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log(documents.length);
+        // console.log("test");
+        res.render('index', { title: 'Home Page' , test: documents });
+      }
+    });
+  })
+});
+router.post('/', function(req,res) {
+  var temp =req.body.txt;
+  JSON.stringify(temp);
+  // console.log(temp);
+  if(temp !== null && temp !== '' && temp !== undefined) {
+    mongo.connect(url, function(err,client) {
+      assert.equal(null,err);
+      var db = client.db('igemConcordia2020');
+      db.collection(collection).find({ "Platform_ORF" : temp }).toArray((err,documents) => {
+        if(err){
+          console.log(err);
+          client.close();
+        } else {
+          if(documents === undefined || documents.length === 0 || documents.length === null) {
+            console.log("No data for this ORF");
+            console.log("Now render to new page and display no data available");
+            client.close();
+            var message = "Nothing was found";
+            res.render('search', {test: message});
+          } else {
+            console.log("Data available!");
+            console.log("Now render to new page and display data");
+            // console.log(documents);
+            client.close();
+            res.render('search', {test: documents});
+          }
+        }
+      })
+    });
+  }
 });
 
 // testInsertToDB();
-
 function testInsertToDB() {
   var test = {
     Name: "Maher",
@@ -24,6 +69,51 @@ function testInsertToDB() {
       assert.equal(null,err);
       console.log("Inserted to DB successfully");
       client.close();
+    });
+  });
+}
+function insertNewColumn_GSE4136(){
+  mongo.connect(url,function(err,client) {
+    assert.equal(null,err);
+    var db = client.db('igemConcordia2020');
+    db.collection(collection).update({},
+        {$set : {"AccessionNumber":"GSE4136" }},
+        {upsert:false,
+          multi:true});
+    console.log("added new column")
+    // Client.close();
+  })
+}
+function addAccessionGSE4136(){
+  mongo.connect(url, function(err, client) {
+    assert.equal(null,err);
+    var db = client.db('igemConcordia2020');
+    db.collection(collection).count((err, count) => {
+      if(err) {
+        console.log("Error in fetching from DB");
+        client.close();
+      } else {
+        if (count !== 0 && !err) {
+          db.collection(collection).find({ "AccessionNumber" : "GSE4136" }).toArray((err,documents) => {
+            if(err){
+                console.log(err);
+                client.close();
+            } else {
+              if(documents === undefined || documents.length == null || documents.length === 0) {
+                console.log("AccessionNumber is not in DB! add column!");
+                insertNewColumn_GSE4136();
+                client.close();
+              } else {
+                console.log("column already exists for this study");
+                client.close();
+              }
+            }
+          })
+        } else {
+          console.log("GSE4136 column is already added");
+          client.close();
+        }
+      }
     });
   });
 }
