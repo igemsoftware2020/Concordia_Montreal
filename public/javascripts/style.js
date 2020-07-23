75eb3bfaf9766663c8689aa8688628c7bf45e4ee
MaherHassanain <maher_hassanain@hotmail.com>
2020-07-23 15:43:16 -0400

    version 3.0 with basic frontend template view

---
+++ public/javascripts/style.js
@@ -0,0 +1,70 @@
+// <script>
+$(document).ready(function(){
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#main']").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    $(window).scroll(function() {
+        $(".slideanim").each(function(){
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+})
+
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("BTNT");
+    if (x.innerHTML === "Show More Options") {
+        x.innerHTML = "Show Less Options";
+    } else {
+        x.innerHTML = "Show More Options";
+    }
+}
+
+$(document).ready(function(){
+    $("button2").click(function(){
+        $("p2").toggle();
+    });
+});
---
+++ routes/GSE64468_meta.js
@@ -0,0 +1,41 @@
+//===========================================================================
+//						GSE64468_meta.js
+//				  Developed by Maher Hassanain
+//						 July 2020
+//===========================================================================
+const mongo = require('mongodb');
+var assert = require('assert');
+var url = 'mongodb://localhost:27017';
+const collection = "metaData";
+const DSE4136DATA = require('../JSON/GSE64468_meta_Data');
+
+
+function insert_GSE64468_Yeast_Data_meta(){
+    mongo.connect(url, function(err, client) {
+        assert.equal(null,err);
+        var db = client.db('igemConcordia2020');
+        db.collection(collection).count((err, count) => {
+            if(err) {
+                console.log("Error in fetching from DB");
+            } else {
+                if (count === 0 && !err) {
+                    console.log("Collection for GSE4136 meta-data is empty, insert to DB");
+                    db.collection(collection).insertMany(DSE4136DATA, function(err, results) {
+                        if (err) {
+                            console.log("Error in inserting GSE4136 Data to DB");
+                            console.log(err);
+                        } else {
+                            console.log("GSE4136 Data inserted successfully to DB");
+                            client.close();
+                        }
+                    });
+                } else {
+                    console.log("GSE4136 Data is already filled, keep as it is");
+                    client.close();
+                }
+            }
+        });
+    });
+}
+
+module.exports = insert_GSE64468_Yeast_Data_meta();
